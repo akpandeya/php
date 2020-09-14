3a6a4a4f87e256a689fe0dda17eddc9eb1b6816f
akpandeya <akpandeya1@gmail.com>
2020-09-14 21:53:07 +0200

    first commit

---
+++ lesson1/02.web-server.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+
+        <?php
+            echo $_SERVER['REMOTE_HOST'];
+        ?>
+
+    </body>
+</html>
